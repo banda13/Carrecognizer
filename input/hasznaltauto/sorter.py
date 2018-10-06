54210abd410d95e6809c95b315845ce1348528f2
Szabó András <szabag@gmail.com>
2018-10-06 17:11:46 +0200

    hasznlatauto data collector + sorter

---
+++ input/hasznaltauto/sorter.py
@@ -0,0 +1,41 @@
+import os
+import random
+
+from shutil import copy
+
+def sort_train_vs_text(p_train, p_test, min_limit):
+    root_dir = "../../data/hasznaltauto/"
+    train_dir = "../../data/train/"
+    test_dir = "../../data/test/"
+
+    categories = os.listdir(root_dir)
+    print("%d category founded" %len(categories))
+    for category in categories:
+        files_in_category = os.listdir(root_dir + category)
+
+        if len(files_in_category) < min_limit:
+            print("Category %s skipped because of the lack of images: %d" %(category, len(files_in_category)))
+            continue
+
+        category_length = len(files_in_category)
+        random.shuffle(files_in_category)
+
+        train_count = int(p_train * category_length)
+        test_count = int(p_test * category_length)
+
+        print("%d train and %d test data determined in category %s" % (train_count, test_count, category))
+
+        if not os.path.exists(train_dir + category + "/"):
+            os.makedirs(train_dir + category + "/")
+        if not os.path.exists(test_dir + category + "/"):
+            os.makedirs(test_dir + category + "/")
+
+        for i in range(category_length):
+            if i <= train_count:
+                copy(root_dir + category + "/" + files_in_category[i], train_dir + category)
+            else:
+                copy(root_dir + category + "/" + files_in_category[i], test_dir + category)
+
+        print("%s category done" % category)
+
+sort_train_vs_text(0.8, 0.2, 200)
