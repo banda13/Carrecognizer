382ea5831fe5df3c3faf79926270a38e0514486e
Szabó András <szabag@gmail.com>
2018-10-09 15:33:38 +0200

    save model history + refactor

--- classifiers/cnn2.py
+++ classifiers/cnn2.py
@@ -1,6 +1,10 @@
 import os
+import json
 import time
 import datetime
+import uuid
+
+from PIL import ImageFile
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,11 +13,16 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Input, Lambda, Dense, BatchNormalization, Flatten
 from tensorflow.python.keras.layers import Convolution2D, MaxPooling2D, Dropout
 from tensorflow.python.keras.optimizers import Adam, SGD
+from keras import regularizers
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
 
 from keras.models import load_model
 
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
+
+from test import safer_manual_accuracy_test
 
 
 class Cnn2(object):
@@ -21,10 +30,11 @@ class Cnn2(object):
     def __init__(self):
         self.classifier = None
         self.history = None
-        self.name = "cnn_model"
+        self.id = uuid.uuid4()
+        self.name = "cnn_model" + str(self.id)
 
         # CNN PROPERTIES
-        self.learning_rate = 0.0001 # TODO slow down learing rate, check results (0.001 to big maybe)
+        self.learning_rate = 0.0001  # TODO slow down learing rate, check results (0.001 to big maybe)
 
     def build(self):
         self.classifier = Sequential()
@@ -48,11 +58,12 @@ class Cnn2(object):
         self.classifier.add(Dropout(0.5))
 
         # TODO még 1 Dense réteg is lehet segít hogy bonyolultabban közeltsen a háló
-        self.classifier.add(Dense(1, activation='sigmoid'))
+        self.classifier.add(Dense(1, kernel_regularizer=regularizers.l2(0.01), activation='sigmoid'))
+        # TODO l2 regularizáció vagy esetleg early stopping is jó lehet h elkerüljük az overfittinget
 
         # TODO try SGD optimalizer pl:
         # SGD(lr=1e-4, momentum=0.9)
-        self.classifier.compile(optimizer=Adam(lr = self.learning_rate), loss='binary_crossentropy', metrics=['accuracy'])
+        self.classifier.compile(optimizer=Adam(lr=self.learning_rate), loss='binary_crossentropy', metrics=['accuracy'])
         print("Model build")
 
     def train(self, continuous_train=False):
@@ -61,6 +72,7 @@ class Cnn2(object):
         else:
             self.build()
 
+        ImageFile.LOAD_TRUNCATED_IMAGES = True
         image_gen = ImageDataGenerator(rescale=1.0 / 255)
 
         train_set = image_gen.flow_from_directory("data/train/",
@@ -76,8 +88,8 @@ class Cnn2(object):
         try:
             self.history = self.classifier.fit_generator(
                 train_set,
-                steps_per_epoch=1000,  # 8000
-                #TODO try more epoch with slow learning rate
+                steps_per_epoch=100,  # 8000
+                # TODO try more epoch with slow learning rate
                 epochs=5,  # 10
                 validation_data=test_set,
                 validation_steps=800  # 800
@@ -86,10 +98,16 @@ class Cnn2(object):
             if continuous_train:
                 self.save()
 
-    def visualize_history(self):
+        self.save()
+        self.evaluate()
 
+    def evaluate(self):
         if self.history is None:
-            raise Exception("Train the model first before visualize history")
+            raise Exception("Train the model before evaluate it")
+        manual_accuracy = safer_manual_accuracy_test(self)
+        self.history.history['manual_acc'] = manual_accuracy
+        with open('statistics/cnn_ ' + self.name + '.json', 'w') as outfile:
+            json.dump(self.history.history, outfile, indent=4)
 
         plt.figure()
         plt.plot(self.history.history['loss'], label="train_loss")
@@ -103,8 +121,8 @@ class Cnn2(object):
         plt.ylabel("Loss/Accuracy")
         plt.legend()
 
-        plt.savefig("statistics/cnn_" + str(time.time()) + ".png")
-        plt.show()
+        plt.savefig("statistics/" + self.name + ".png")
+        # plt.show()
 
     def use(self, img_name):
         if self.classifier is None:
@@ -117,7 +135,12 @@ class Cnn2(object):
         asd = np.argmax(prediction)
         asd2 = prediction[0][0]
         return asd2
-        #return np.argmax(prediction)
+        # return np.argmax(prediction)
+
+    def show_the_model(self):
+        if self.classifier is None:
+            self.build()
+        SVG(model_to_dot(self.classifier).create(prog='dot', format='svg'))
 
     def clear(self):
         if self.classifier is not None:
