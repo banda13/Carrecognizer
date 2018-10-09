cf8ab1da91f879a349d98c7b56b3b79ed363d562
Szabó András <szabag@gmail.com>
2018-10-09 15:51:35 +0200

    sorter fix: classes equal cardinality

--- input/hasznaltauto/sorter.py
+++ input/hasznaltauto/sorter.py
@@ -3,25 +3,26 @@ import random
 
 from shutil import copy
 
-def sort_train_vs_text(p_train, p_test, min_limit):
+
+def sort_train_vs_text(p_train, p_test, limit):
     root_dir = "../../data/hasznaltauto/"
     train_dir = "../../data/train/"
     test_dir = "../../data/test/"
 
     categories = os.listdir(root_dir)
-    print("%d category founded" %len(categories))
+    print("%d category founded" % len(categories))
     for category in categories:
         files_in_category = os.listdir(root_dir + category)
 
-        if len(files_in_category) < min_limit:
-            print("Category %s skipped because of the lack of images: %d" %(category, len(files_in_category)))
+        if len(files_in_category) < limit:
+            print("Category %s skipped because of the lack of images: %d" % (category, len(files_in_category)))
             continue
 
         category_length = len(files_in_category)
         random.shuffle(files_in_category)
 
-        train_count = int(p_train * category_length)
-        test_count = int(p_test * category_length)
+        train_count = int(p_train * limit)
+        test_count = int(p_test * limit)
 
         print("%d train and %d test data determined in category %s" % (train_count, test_count, category))
 
@@ -31,11 +32,12 @@ def sort_train_vs_text(p_train, p_test, min_limit):
             os.makedirs(test_dir + category + "/")
 
         for i in range(category_length):
-            if i <= train_count:
+            if i <= limit:
                 copy(root_dir + category + "/" + files_in_category[i], train_dir + category)
             else:
                 copy(root_dir + category + "/" + files_in_category[i], test_dir + category)
 
         print("%s category done" % category)
 
-sort_train_vs_text(0.8, 0.2, 200)
+
+sort_train_vs_text(0.8, 0.2, 25000)
