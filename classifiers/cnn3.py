d469a721a0c9c3e598948108f859f3da94323fa5
Szabó András <szabag@gmail.com>
2018-10-10 21:44:53 +0200

    refactor multiclass cnn params & evaluate train statistics
    Former-commit-id: b82e6a0444e1ac4bf938d032851886cdbb5f15ab

--- classifiers/cnn3.py
+++ classifiers/cnn3.py
@@ -9,20 +9,35 @@ import matplotlib.pyplot as plt
 import math
 import cv2
 import uuid
+import json
 from PIL import ImageFile
+
 
 class Cnn3(object):
 
     def __init__(self):
         self.img_width, self.img_height = 224, 224
         self.id = uuid.uuid4()
-
-        self.top_model_weights = 'classifiers/pre_train/bottleneck_' + str(self.id) + ".h5"
-        self.epochs = 25
-        self.batch_size = 16
-
         self.train_dir = "data/train/"
         self.test_dir = "data/test/"
+
+        # VGG16
+        self.bottleneck_test_features = "model/bottleneck/bottleneck_features_train.npy"
+        self.bottleneck_train_features = "model/bottleneck/bottleneck_features_validation.npy"
+        self.bottleneck_model = "model/bottleneck/bigmodel.h5"
+
+        # TOP model
+
+        self.top_model_weights = 'classifiers/pre_train/bottleneck_' + str(self.id) + ".h5"
+        self.class_indices = "model/" + str(self.id) + "class_indices.npy"
+
+        # TRAIN params
+        self.epochs = 25
+        self.batch_size = 16
+        self.learning_rate = 0.00001
+
+        self.model = None
+        self.history = None
 
     def save_bottlebeck_features(self):
         # build the VGG16 network
@@ -50,7 +65,7 @@ class Cnn3(object):
         bottleneck_features_train = model.predict_generator(
             generator, predict_size_train)
 
-        np.save('bottleneck_features_train.npy', bottleneck_features_train)
+        np.save(self.bottleneck_train_features, bottleneck_features_train)
 
         generator = datagen.flow_from_directory(
             self.test_dir,
@@ -67,15 +82,11 @@ class Cnn3(object):
         bottleneck_features_validation = model.predict_generator(
             generator, predict_size_validation)
 
-        np.save('classifiers/pre_train/bottleneck_features_validation.npy',
-                bottleneck_features_validation)
-
-        try:
-            model.save("bigmodel.h5")
-        except Exception as e:
-            print("nem jött össze")
+        np.save(self.bottleneck_test_features, bottleneck_features_validation)
+        model.save(self.bottleneck_model)
 
     def train_top_model(self):
+
         ImageFile.LOAD_TRUNCATED_IMAGES = True
         datagen_top = ImageDataGenerator(rescale=1. / 255)
         generator_top = datagen_top.flow_from_directory(
@@ -89,9 +100,9 @@ class Cnn3(object):
         num_classes = len(generator_top.class_indices)
 
         # save the class indices to use use later in predictions
-        np.save('class_indices.npy', generator_top.class_indices)
+        np.save(self.class_indices, generator_top.class_indices)
 
-        train_data = np.load('bottleneck_features_train.npy')
+        train_data = np.load(self.bottleneck_train_features)
 
         train_labels = generator_top.classes
 
@@ -106,48 +117,52 @@ class Cnn3(object):
 
         nb_validation_samples = len(generator_top.filenames)
 
-        validation_data = np.load('classifiers/pre_train/bottleneck_features_validation.npy')
+        validation_data = np.load(self.bottleneck_test_features)
 
         validation_labels = generator_top.classes
         validation_labels = to_categorical(
             validation_labels, num_classes=num_classes)
 
-        model = Sequential()
-        model.add(Flatten(input_shape=train_data.shape[1:]))
-        model.add(Dense(256, activation='relu'))
-        model.add(Dropout(0.5))
-        model.add(Dense(num_classes, activation='softmax'))
+        self.model = Sequential()
+        self.model.add(Flatten(input_shape=train_data.shape[1:]))
+        self.model.add(Dense(256, activation='relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(num_classes, activation='softmax'))
 
-        model.compile(optimizer=RMSprop(lr=0.00001),
-                      loss='categorical_crossentropy', metrics=['accuracy'])
+        self.model.compile(optimizer=RMSprop(lr=self.learning_rate),
+                           loss='categorical_crossentropy', metrics=['accuracy'])
 
-        history = model.fit(train_data, train_labels,
-                            epochs=self.epochs,
-                            batch_size=self.batch_size,
-                            validation_data=(validation_data, validation_labels))
+        self.history = self.model.fit(train_data, train_labels,
+                                      epochs=self.epochs,
+                                      batch_size=self.batch_size,
+                                      validation_data=(validation_data, validation_labels))
 
-        model.save_weights(self.top_model_weights)
+        self.model.save_weights(self.top_model_weights)
 
-        (eval_loss, eval_accuracy) = model.evaluate(
+        (eval_loss, eval_accuracy) = self.model.evaluate(
             validation_data, validation_labels, batch_size=self.batch_size, verbose=1)
 
         print("Accuracy: {:.2f}%".format(eval_accuracy * 100))
         print("Loss: {}".format(eval_loss))
 
+    def evaluate(self):
         plt.figure(1)
         plt.subplot(211)
-        plt.plot(history.history['acc'])
-        plt.plot(history.history['val_acc'])
+        plt.plot(self.history.history['acc'])
+        plt.plot(self.history.history['val_acc'])
         plt.title('model accuracy')
         plt.ylabel('accuracy')
         plt.xlabel('epoch')
         plt.legend(['train', 'test'], loc='upper left')
 
         plt.subplot(212)
-        plt.plot(history.history['loss'])
-        plt.plot(history.history['val_loss'])
+        plt.plot(self.history.history['loss'])
+        plt.plot(self.history.history['val_loss'])
         plt.title('model loss')
         plt.ylabel('loss')
         plt.xlabel('epoch')
         plt.legend(['train', 'test'], loc='upper left')
         plt.savefig("statistics/" + str(self.id) + ".png")
+
+        with open('statistics/' + str(self.id) + '.json', 'w') as outfile:
+            json.dump(self.history.history, outfile, indent=4)
