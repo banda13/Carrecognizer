f906e295762d615fb46a9df06a82c6f1e986bd34
Szabó András <szabag@gmail.com>
2018-10-13 20:08:36 +0200

    teszt main
    Former-commit-id: 82b90ebb7367d2c5ee60fa03b9fbbfd6c70dc0c3

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from classifiers.cnn3 import Cnn3
 from random import sample
 import os
 
+from classifiers.cnn_test import cnn3_test
+
+
 def test():
     paths = ["data/hasznaltauto/bmw/", "data/hasznaltauto/ford/", "data/hasznaltauto/mercedes/",
              "data/hasznaltauto/volkswagen/"]
@@ -10,5 +13,19 @@ def test():
         for car in sample(os.listdir(path), 3):
             Cnn3("e7247c4f-88c1-4f0d-bf62-f7850de6d949").predict(path + car, car)
 
-Cnn3("e7247c4f-88c1-4f0d-bf62-f7850de6d949").model_to_graph()
 
+#cnn = Cnn3("e7247c4f-88c1-4f0d-bf62-f7850de6d949")
+#cnn.model_to_graph()
+#cnn.print_graph_nodes("model/bottleneck_graph.pb")
+
+#cnn = Cnn3()
+#cnn.save_bottlebeck_features()
+#cnn.train_top_model()
+#cnn.model_to_graph()
+
+
+cnn = Cnn3()
+# cnn.save_bottlebeck_features()
+tezt = cnn3_test()
+tezt.setup_models()
+tezt.run_tests()
