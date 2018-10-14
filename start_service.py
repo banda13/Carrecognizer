0c99e69e807c89a56e7bf0dd395fc7f665ae584c
Szabó András <szabag@gmail.com>
2018-10-14 13:34:22 +0200

    heroku port
    Former-commit-id: d7c6868c61491d29cd363b1c316a458461f0f419

--- start_service.py
+++ start_service.py
@@ -1,4 +1,5 @@
 #!flask/bin/python
+import os
 from flask import Flask
 
 from services.file_service import file_upload, file_uploader
@@ -10,4 +11,6 @@ app.register_blueprint(test_service, url_prefix='/test')
 app.register_blueprint(file_upload, url_prefix='/file')
 app.register_blueprint(file_uploader, url_prefix='/file')
 
-app.run()
+port = int(os.environ.get("PORT", 5000))
+
+app.run(host='0.0.0.0', port=port)
