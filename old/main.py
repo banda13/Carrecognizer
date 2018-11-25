b7380baa441070cb710750587b98dbea600783ec
Szabó András <szabag@gmail.com>
2018-11-25 01:38:43 +0100

    refactor old unused files
    Former-commit-id: 69af21654ea619e6baa3334f04dd123f47e90dae

---
+++ old/main.py
@@ -0,0 +1,3 @@
+# cnn = Cnn3()
+# cnn.save_bottlebeck_features()
+cnn.train_top_model()
--- utils.py
+++
@@ -1,8 +0,0 @@
-def largest_prime_factor(n):
-    i = 2
-    while i * i <= n:
-        if n % i:
-            i += 1
-        else:
-            n //= i
-    return n
