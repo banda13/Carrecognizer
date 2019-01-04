06df77a8c56a62b768d075ccb337d09e26aaa872
Szabó András <szabag@gmail.com>
2019-01-04 11:22:21 +0100

    paths updated
    Former-commit-id: 62bb0b33f157706ef8e9a7694532f580ca7c8fa8

--- paths.py
+++ paths.py
@@ -1,13 +1,18 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-# TRAIN_DIR = ROOT_DIR + "/data/train/"
-# # TEST_DIR = ROOT_DIR + "/data/test/"
 TRAIN_DIR = ROOT_DIR + "/data/train/"
 TEST_DIR = ROOT_DIR + "/data/test/"
+N_TRAIN_DIR = ROOT_DIR + "/data2/train/"
+N_TEST_DIR = ROOT_DIR + "/data2/test/"
+
 STAT_DIR = ROOT_DIR + "/statistics/"
 HASZNALT_DIR = ROOT_DIR + "/data/hasznaltauto/"
 TRADER_DIR = ROOT_DIR + "/data/autotrader/"
 TRADER_DETAIL_DIR = ROOT_DIR + "/data/autotrader_details/"
 VIT_DIR = ROOT_DIR + "/data/autovit/"
 SCOUT_DIR = ROOT_DIR + "/data/autoscout/"
+
+N_SCOUT_DIR = ROOT_DIR + "/data2/autoscout/"
+N_SCOUT_META_DIR = N_SCOUT_DIR + 'meta/'
+N_SCOUT_IMG_DIR = N_SCOUT_DIR + "images/"
