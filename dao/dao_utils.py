b9d11fd079fd6b732b00a71045696a929aa0ba04
Szabó András <szabag@gmail.com>
2019-01-16 09:11:01 +0100

    load some basic table data from cache (ex: models makes)
    Former-commit-id: 3a8f953106b2de51107b15e792c90c84597ed5ac

--- dao/dao_utils.py
+++ dao/dao_utils.py
@@ -23,4 +23,29 @@ def rebuild_db():
     connection.drop_and_create()
 
 
+def load_basic_tables():
+    from dao.base import connection
+    from dao.models.base_tables import Model, Make
 
+    session = connection.open_session()
+    model_data_filename = paths.ROOT_DIR + '/model_backup.csv'
+    make_data_filename = paths.ROOT_DIR + '/make_backup.csv'
+
+    with open(model_data_filename) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            m = Model(row['model'])
+            m.model_id = row['model_id']
+            session.add(m)
+    session.commit()
+    print("Model table data loaded")
+
+    with open(make_data_filename) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            m = Make(row['make'])
+            m.set_make(row['make_id'], row['make'], row['model_id'])
+            session.add(m)
+    session.commit()
+    print("Make table data loaded")
+    session.close()
